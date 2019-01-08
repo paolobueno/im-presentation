c5dcf5065d13f26986a762e4b92d0b66a34b864b
Paolo Bueno <paolohaji@gmail.com>
2019-01-08 10:00:22 +0100

    chore: Use react alpha version for Hooks

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+module.exports = {
+  resolve: {
+    alias: {
+      react: path.resolve(__dirname, 'node_modules/react'),
+      'react-dom': path.resolve(__dirname, 'node_modules/react-dom'),
+    },
+  },
+};
