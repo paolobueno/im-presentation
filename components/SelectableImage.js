346f3c8c5c340839c5237029b8eb7be9b2f263ca
Paolo Bueno <paolohaji@gmail.com>
2019-01-08 11:05:01 +0100

    feat: extract img clicked row

--- components/SelectableImage.js
+++ components/SelectableImage.js
@@ -1,26 +1,60 @@
-import React, {Fragment, Component} from 'react';
-import {getClickCoords} from './utils';
+import {lensProp, view, set} from 'ramda';
+import React, {Fragment} from 'react';
+import {getClickCoords, lensSetter} from './utils';
 
-class SelectableImage extends Component {
-  constructor() {
-    super();
-    this.state = {
-      coords: [null, null],
+const coordsLens = lensProp('coords');
+
+class SelectableImage extends React.Component {
+  state = set(coordsLens, [0, 0], {});
+  updateCoords = lensSetter(coordsLens);
+
+  canvas = React.createRef();
+  componentDidMount() {
+    const {src} = this.props;
+    const cnv = this.canvas.current;
+    const ctx = cnv.getContext('2d');
+    const img = new Image(cnv.width, cnv.height);
+    img.crossOrigin = '';
+    img.src = src;
+    img.onload = () => {
+      ctx.drawImage(img, 0, 0, img.width, img.height);
     };
   }
-  updateCoords = (coords) => {
-    this.setState({coords});
-  }
+
   render() {
-    const {coords} = this.state;
+    const {width, height, imageStyle, onClick, ...props} = this.props;
+    const coords = view(coordsLens, this.state);
     return (
       <Fragment>
-        <svg width={0} height={0}>
-          <filter>
-            <feFlood />
-          </filter>
-        </svg>
-        <img {...this.props} onClick={e => this.updateCoords(getClickCoords(e))} />
+        <div
+          style={{
+            width,
+            backgroundColor: 'magenta',
+            height: 1,
+            position: 'relative',
+            top: coords[1],
+            transition: 'top 0.3s',
+          }}
+        />
+        <canvas
+          ref={this.canvas}
+          width={width}
+          height={height}
+          onClick={e => {
+            const coords = getClickCoords(e);
+            this.updateCoords(coords);
+
+            const [_, y] = coords;
+            const cnv = this.canvas.current;
+            const {width, height} = cnv;
+            const ctx = cnv.getContext('2d');
+            ctx.drawImage(this.canvas.current, 0, 0);
+            const imgData = ctx.getImageData(0, 0, width, height);
+            const dataRow = imgData.data.slice(y * width * 4, (y + 1) * width * 4);
+            onClick && onClick(dataRow);
+          }}
+          {...props}
+        />
       </Fragment>
     );
   }
--- components/utils.js
+++ components/utils.js
@@ -1,4 +1,12 @@
+import {set} from 'ramda';
+
 export const getClickCoords = e => {
   const {left, top} = e.target.getBoundingClientRect();
   return [Math.round(e.clientX - left), Math.round(e.pageY - top)];
 };
+
+export function lensSetter(lens) {
+  return function(value) {
+    this.setState(set(lens, value, this.state));
+  };
+}
