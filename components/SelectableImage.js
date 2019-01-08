3cb208605bd0823d64ccedca84544f60f79a118f
Paolo Bueno <paolohaji@gmail.com>
2019-01-08 09:51:49 +0100

    Add SelectableImage

--- components/SelectableImage.js
+++ components/SelectableImage.js
@@ -1,17 +1,29 @@
-import React, {Fragment, useState} from 'react';
+import React, {Fragment, Component} from 'react';
 import {getClickCoords} from './utils';
 
-export default ({onSelect, ...props}) => {
-  const [coords, updateCoords] = useState([null, null]);
-  console.log(coords);
-  return (
-    <Fragment>
-      <svg width={0} height={0}>
-        <filter>
-          <feFlood />
-        </filter>
-      </svg>
-      <img {...props} onClick={e => updateCoords(getClickCoords(e))} />
-    </Fragment>
-  );
-};
+class SelectableImage extends Component {
+  constructor() {
+    super();
+    this.state = {
+      coords: [null, null],
+    };
+  }
+  updateCoords = (coords) => {
+    this.setState({coords});
+  }
+  render() {
+    const {coords} = this.state;
+    return (
+      <Fragment>
+        <svg width={0} height={0}>
+          <filter>
+            <feFlood />
+          </filter>
+        </svg>
+        <img {...this.props} onClick={e => this.updateCoords(getClickCoords(e))} />
+      </Fragment>
+    );
+  }
+}
+
+export default SelectableImage;
