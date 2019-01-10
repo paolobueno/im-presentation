995451c69aa49b495e4f0fbc6777c8809ed9a7db
Paolo Bueno <paolohaji@gmail.com>
2019-01-10 16:17:17 +0100

    feat: vertical support

--- components/ScanImage.js
+++ components/ScanImage.js
@@ -1,8 +1,24 @@
-import {lensProp, view, set} from 'ramda';
-import React, {Fragment} from 'react';
+import {lensProp, set, view} from 'ramda';
+import React from 'react';
 import {getClickCoords, lensSetter} from '../utils';
 
 const coordsLens = lensProp('coords');
+
+const Line = ({top, left, vertical}) => (
+  <div
+    style={{
+      width: vertical ? 1 : '100%',
+      height: vertical ? '100%' : 1,
+      backgroundColor: 'magenta',
+      position: 'absolute',
+      display: 'inline',
+      top: vertical ? 0 : top,
+      left: vertical ? left : 0,
+      transition: 'all, 0.3s',
+      zIndex: 10,
+    }}
+  />
+);
 
 class ScanImage extends React.Component {
   state = set(coordsLens, [0, 0], {});
@@ -58,42 +74,43 @@ class ScanImage extends React.Component {
   };
 
   render() {
-    const {width, height, imageStyle, onData, ...props} = this.props;
+    const {width, height, onData, onDoubleClick, vertical, ...props} = this.props;
     const coords = view(coordsLens, this.state);
     return (
-      <div>
-        {coords[1] ? (
-          <div
-            style={{
-              width,
-              backgroundColor: 'magenta',
-              height: 1,
-              position: 'relative',
-              top: coords[1],
-              transition: 'top 0.3s',
-            }}
-          />
-        ) : null}
+      <div style={{width, height, position: 'relative'}} onDoubleClick={onDoubleClick}>
+        {coords[0] ? <Line top={coords[1]} left={coords[0]} vertical={vertical} /> : null}
         <canvas
           ref={this.canvas}
           width={width}
           height={height}
+          style={{position: 'absolute'}}
           onClick={e => {
             const coords = getClickCoords(e);
             this.updateCoords(coords);
 
             if (typeof onData === 'function') {
-              const [_, y] = coords;
+              const [x, y] = coords;
               const cnv = this.canvas.current;
               const {width, height} = cnv;
               const ctx = cnv.getContext('2d');
               ctx.drawImage(this.canvas.current, 0, 0);
               const imgData = ctx.getImageData(0, 0, width, height);
-              const dataRow = imgData.data.slice(
-                (y - 1) * imgData.width * 4,
-                y * imgData.width * 4,
-              );
-              onData(dataRow);
+              if (!vertical) {
+                const dataRow = imgData.data.slice(
+                  (y - 1) * imgData.width * 4,
+                  y * imgData.width * 4,
+                );
+                onData(dataRow);
+              } else {
+                const data = [];
+                for (let i = x * 4; i < imgData.data.length; i += imgData.width * 4) {
+                  data.push(imgData.data[i]);
+                  data.push(imgData.data[i + 1]);
+                  data.push(imgData.data[i + 2]);
+                  data.push(imgData.data[i + 3]);
+                }
+                onData(data);
+              }
             }
           }}
           {...props}
--- slides/ImagesAsFunctions.js
+++ slides/ImagesAsFunctions.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {Line, LineChart} from 'recharts';
+import {Line, LineChart, YAxis, XAxis} from 'recharts';
 import ScanImage from '../components/ScanImage';
 
 const width = 800;
@@ -21,6 +21,7 @@ const processPxArray = pixels => {
 
 export default () => {
   const [levels, setLevels] = useState([]);
+  const [vertical, setVertical] = useState(false);
   const lineOpts = {
     dot: false,
     animationDuration: 300,
@@ -31,9 +32,18 @@ export default () => {
         src="assets/fruits.jpg"
         width={width}
         height={height}
+        vertical={vertical}
+        onDoubleClick={() => setVertical(!vertical)}
         onData={pixels => setLevels(processPxArray(pixels))}
       />
-      <LineChart data={levels} width={width * 0.75} height={height}>
+      <LineChart
+        layout={vertical ? 'vertical' : 'horizontal'}
+        data={levels}
+        width={width * 0.7}
+        height={height}
+      >
+        <YAxis type={vertical ? 'category' : 'number'} hide="true" />
+        <XAxis type={vertical ? 'number' : 'category'} hide="true" />
         <Line dataKey="r" {...lineOpts} stroke="red" />
         <Line dataKey="g" {...lineOpts} stroke="green" />
         <Line dataKey="b" {...lineOpts} stroke="blue" />
