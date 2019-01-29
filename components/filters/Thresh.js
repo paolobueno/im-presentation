78913a69b20d027fb2da7f6d46f876418079334f
Paolo Bueno <paolohaji@gmail.com>
2019-01-29 17:05:02 +0100

    feat: add in and out to thresh

--- components/filters/Thresh.js
+++ components/filters/Thresh.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-export default ({thresh}) => {
+export default ({thresh, inGraphic, outGraphic = 'thresh'}) => {
   const table = '0 '.repeat(thresh) + '1 '.repeat(256 - thresh);
   return (
-    <feComponentTransfer>
+    <feComponentTransfer in={inGraphic} out={outGraphic}>
       <feFuncR type="discrete" tableValues={table} />
       <feFuncG type="discrete" tableValues={table} />
       <feFuncB type="discrete" tableValues={table} />
